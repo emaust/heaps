0aa80b42c260fb3cec9cdba33b0fee69e63dbc3c
Erika Maust <jamjar@Erikas-MacBook-Pro.local>
2020-10-01 20:07:19 -0700

    min heap passing

--- lib/min_heap.rb
+++ lib/min_heap.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class HeapNode
   attr_reader :key, :value
 
@@ -17,7 +19,11 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def add(key, value = key)
-    raise NotImplementedError, "Method not implemented yet..."
+    
+    @store.push(HeapNode.new(key,value))
+    child = @store.length - 1
+    heap_up(child) if child != 0 
+
   end
 
   # This method removes and returns an element from the heap
@@ -25,7 +31,13 @@ class MinHeap
   # Time Complexity: ?
   # Space Complexity: ?
   def remove()
-    raise NotImplementedError, "Method not implemented yet..."
+
+    swap(0, @store.length - 1)
+    deleted = @store.pop
+    heap_down(0) unless self.empty?
+
+    return deleted.value
+
   end
 
 
@@ -47,7 +59,7 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def empty?
-    raise NotImplementedError, "Method not implemented yet..."
+    return false unless @store[0].nil?
   end
 
   private
@@ -58,14 +70,42 @@ class MinHeap
   # Time complexity: ?
   # Space complexity: ?
   def heap_up(index)
-    
+
+    parent = (index - 2) / 2 if index.even?
+    parent = (index - 1) / 2 if index.odd?
+
+    if @store[parent].key > @store[index].key
+      swap(parent, index)
+      heap_up(parent) if parent != 0
+    end
+
   end
 
   # This helper method takes an index and 
   #  moves it up the heap if it's smaller
   #  than it's parent node.
   def heap_down(index)
-    raise NotImplementedError, "Method not implemented yet..."
+  
+    left = (2 * index) + 1
+    right = (2 * index) + 2
+
+
+    if !@store[right].nil?
+      min_child = left if @store[left].key < @store[right].key
+      min_child = right if @store[left].key > @store[right].key
+    elsif @store[left].nil?
+      return
+    else
+      min_child = left
+    end
+
+    if @store[index].key > @store[min_child].key
+      swap(index, min_child)
+      index = min_child
+      heap_down(index)
+    end
+
+
   end
 
   # If you want a swap method... you're welcome
